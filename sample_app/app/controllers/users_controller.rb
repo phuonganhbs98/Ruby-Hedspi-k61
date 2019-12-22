357c77ebd00258e015710f703193289e1c034d24
phuonganh <phuonganhbs1312@gmail.com>
2019-12-22 22:57:29 +0700

    Update chapter 10

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+    before_action :admin_user, only: :destroy
     def show
         @user = User.find(params[:id])
     end
@@ -22,6 +24,46 @@ class UsersController < ApplicationController
 
     def edit
         @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            #Handle a successful update.
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+
+    def logged_in_user
+        unless logged_in?
+            store_location
+            flash[:danger] = "Please log in."
+            redirect_to login_url
+        end
+    end
+
+    #Confirms the correct user.
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def index
+        @user = User.paginate(page: params[:page])
+    end
+
+    def destroy
+        User.find(params[:id]).destroy
+        flash[:success] = "User deleted"
+        redirect_to users_url
+    end
+
+    #Confirms an admin user
+    def admin_user
+        redirect_to(root_url) unless current_user.admin?
     end
 
     private
