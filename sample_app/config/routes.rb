9848b2f1a694b710106e2505a4395eb99b9f4d2a
phuonganh <phuonganhbs1312@gmail.com>
2019-12-22 17:16:13 +0700

    Update chapter 8,9

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  get 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
   resources :users
 end
