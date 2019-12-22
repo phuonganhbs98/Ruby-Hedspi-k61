a2dd6385956a9ddc51036dbc5cd563baa45954dd
phuonganh <phuonganhbs1312@gmail.com>
2019-12-23 00:39:32 +0700

    Update chapter 12

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
@@ -9,4 +11,5 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
   resources :users
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
