7a7daed12f8d6cef46cef931ba6f1886f840e61a
phuonganh <phuonganhbs1312@gmail.com>
2019-12-23 13:30:20 +0700

    Update chapter 13

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy'
   resources :users
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only:[:create, :destroy]
 end
